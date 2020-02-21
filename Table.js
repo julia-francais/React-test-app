99354b25ecd99186a1478040934a1eac89ed8108
julia-francais <ju.francais@gmail.com>
2020-02-21 16:52:23 +0100

    submitform added

--- Table.js
+++ Table.js
@@ -1,5 +1,37 @@
 import React from 'react'
 
+// SIMPLE COMPONENT 
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = props => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
+            </tr>
+        );
+    });
+
+    return (
+        <tbody>
+            {rows}
+        </tbody>
+    )
+}
+
 //CUSTOM CLASS COMPONENT
 const Table = (props) => {
     const { characterData, removeCharacter } = props;
@@ -12,35 +44,4 @@ const Table = (props) => {
     );
 }
 
-// SIMPLE COMPONENT 
-const TableHeader = () => {
-    return (
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Job</th>
-            </tr>
-        </thead>
-    )
-}
-
-const TableBody = (props) => {
-    const rows = props.characterData.map((row, index) => {
-        return (
-            <tr key={index}>
-                <td>{row.name}</td>
-                <td>{row.job}</td>
-                <td>
-                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
-                </td>
-            </tr>
-        )
-    })
-    return (
-        <tbody>
-            {rows}
-        </tbody>
-    )
-}
-
-export default Table
+export default Table;
